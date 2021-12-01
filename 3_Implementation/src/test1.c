46dbd6d2d934fb0173d57e031edd3243cadd3104
Pitambar21 <94235986+Pitambar21@users.noreply.github.com>
2021-12-01 17:56:12 +0530

    Test

---
+++ 3_Implementation/src/test1.c
@@ -0,0 +1,29 @@
+/**
+ * @file Test1.c
+ * @author Pitambar
+ * @brief Chanfes the state of LED
+ * @version 0.1
+ * @date 2021-12-01
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+// include files
+#include<avr/io.h>
+#include "Test1.h"
+/**
+ * @brief Change the state of the LED Pin according to the value of state
+ *
+ * @param state Pin level to which the LED Pin should be set
+ */
+void ledstat(uint8_t state)
+{
+	LED_PORT = (state << LED_PIN);
+}
+void InitLED(void)
+{
+	DDRB|=(1<<PB0);
+    DDRD&=~(1<<PD1);
+    PORTD|=(1<<PD1);
+    PORTD|=(1<<PD4);
+}
